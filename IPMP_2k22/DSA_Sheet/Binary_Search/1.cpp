5fb8516e56ebc1c55f9b6fc28e78b11267aec5fe
KkayCodes <154241029+KkayCodes@users.noreply.github.com>
2024-01-01 12:40:57 +0530

    Create 1.cpp

---
+++ IPMP_2k22/DSA_Sheet/Binary_Search/1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int search(const vector<int>& arr, int key) {
+    int low = 0;
+    int high = arr.size() - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == key) {
+            return mid;
+        }
+        if (arr[low] <= arr[mid]) {
+            if (arr[low] <= key && key < arr[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        } else {  
+            if (arr[mid] < key && key <= arr[high]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    vector<int> arr = {4, 5, 6, 7, 8, 9, 1, 2, 3};
+    int key = 6;
+
+    int index = search(arr, key);
+
+    if (index != -1) {
+        cout << "Element " << key << " found at index " << index << endl;
+    } else {
+        cout << "Element " << key << " not found in the array" << endl;
+    }
+
+    return 0;
+}
